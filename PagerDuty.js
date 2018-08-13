37229a6d35b743060d4587f362ee08d43f6f480c
tomg <tom.goldberg@cyberbit.com>
2018-08-13 11:13:31 +0300

    Added Get Services method

--- PagerDuty.js
+++ PagerDuty.js
@@ -4,6 +4,33 @@ class PagerDuty {
         this.serverURL = params.serverURL;
         this.authToken = params.authToken;
     }
+    getServices()
+    {
+        const options = {
+            url: `${this.serverURL}/services`,
+            method: "GET",
+            headers: {
+              Accept: "application/vnd.pagerduty+json;version=2",
+              Authorization: `Token token=${this.authToken}`
+            } ,
+            rejectUnauthorized:false
+          }; 
+          return new Promise(function(resolve,reject){
+              var parsedBody;
+
+              request.get(options,function(err,response,body){
+                  if (err){
+                      reject(err)
+                  }
+                  parsedBody=JSON.parse(body)
+                  if (parsedBody.error)
+                  {
+                      reject(parsedBody.error.message)
+                  }
+                  resolve(parsedBody.services)
+              })
+          })
+    }
     getIncidents() {
         const options = {
             url: `${this.serverURL}/incidents`,
--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var PagerDuty=require('./PagerDuty')
 var pgClient=new PagerDuty({
     //y_NbAkKc66ryYTWUXYEu
     //twbBd56N1ZpDH4i7wbiW
+    //KUAf2tTGCW5cFAxkbduq
     serverURL: "https://api.pagerduty.com",
-    authToken: "twbBd56N1ZpDH4i7wbiW"
+    authToken: "KUAf2tTGCW5cFAxkbduq"
 })
 
 pgClient.getIncidents()
@@ -14,5 +15,16 @@ pgClient.getIncidents()
 .catch(function(ex){
     console.log('Error!: '+ ex)
 })
+
+pgClient.getServices()
+.then(function(services) {
+    console.log(services)
+})
+.catch(function(ex){
+    console.log('Error!: '+ ex)
+})
+
+
+
 
 
